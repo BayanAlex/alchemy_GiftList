e31599842301a63a0e82fdf891bf4a3028d3f8be
Oleksandr Shyhyda <BayanAlex@gmail.com>
2025-02-04 20:14:45 +0200

    Solved the task: - Added proof request on the client - Added proof verification on the server

--- client/index.js
+++ client/index.js
@@ -3,12 +3,16 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
+const usersTree = new MerkleTree(niceList);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const name = process.argv.slice(2).join(' ');
+  const index = niceList.findIndex(n => n === name);
+  const proof = usersTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof
   });
 
   console.log({ gift });
